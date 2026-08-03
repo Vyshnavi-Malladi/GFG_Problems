8b0f25682610acb13c45b9e85041683415084692
Vyshnavi-Malladi <vyshnavimalladi2004@gmail.com>
2026-08-03 10:38:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Words in String/count-words-in-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int countWords(String s) {
+        
+        int count=1;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
